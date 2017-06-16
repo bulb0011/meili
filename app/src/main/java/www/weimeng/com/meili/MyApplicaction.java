a3aff5c08b8735acdc5cfb08cd33c5dbe98aa486
dengpao <386021847@qq.com>
2017-06-16 15:11:37 +0800

    初始化联网

---
+++ app/src/main/java/www/weimeng/com/meili/MyApplicaction.java
@@ -0,0 +1,35 @@
+package www.weimeng.com.meili;
+
+import android.app.Application;
+
+import com.zhy.http.okhttp.OkHttpUtils;
+
+import java.util.concurrent.TimeUnit;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Created by Administrator on 2017/6/16.
+ */
+
+public class MyApplicaction extends Application{
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        //配置okhttp
+        OkHttpClient okHttpClient = new OkHttpClient.Builder()
+//                .addInterceptor(new LoggerInterceptor("TAG"))
+                .connectTimeout(10000L, TimeUnit.MILLISECONDS)
+                .readTimeout(10000L, TimeUnit.MILLISECONDS)
+                //其他配置
+                .build();
+
+        OkHttpUtils.initClient(okHttpClient);
+
+    }
+
+
+}
